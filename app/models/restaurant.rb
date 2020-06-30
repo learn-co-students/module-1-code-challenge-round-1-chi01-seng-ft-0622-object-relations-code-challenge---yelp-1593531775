03abce5861990cbb56dea67f7ff05a467157d9f3
msguttman <msguttman@gmail.com>
2020-06-30 12:12:26 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,41 @@
 class Restaurant
+
+  @@all = []
+
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    customers = []
+    Review.all.map do |review|
+      if review.restaurant == self
+        customers << review.customer
+      end
+    end
+    customers
+  end
+
+  def average_star_rating
+    ratings = []
+    Review.all.select do |review|
+      review.restaurant == self
+      ratings << review.rating
+    end
+    ratings.inject(0.0) { |sum, el| sum + el } / ratings.size
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,26 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+matt = Customer.new("Matt", "Guttman")
+haley = Customer.new("Haley", "Ferro")
+seth = Customer.new("Seth", "Starr")
+
+mcdonalds = Restaurant.new("McDonalds")
+arbys = Restaurant.new("Arbys")
+
+matt_mcdonalds = Review.new(matt, mcdonalds, 10)
+haley_mcdonalds = Review.new(haley, mcdonalds, 5)
+seth_arbys = Review.new(seth, arbys, 7)
+
+
 binding.pry
+
 0 #leave this here to ensure binding.pry isn't the last line
+
+puts "exit pry"
