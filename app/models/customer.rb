77a2576925efccf6bd773ec3efe47318c7738235
github username <github email address>
2020-06-30 12:12:32 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,38 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
-    "#{given_name} #{family_name}"
+    "#{given_name} #{family_name}" #given = first name family = last name
   end
+
+  def self.all
+    @@all
+  end 
+
+  def self.find_by_name(name)
+    name_array = name.split("")
+    Customer.all.find do |person|
+      person.given_name == name_array[0] && person.family_name == name_array[1]
+    end 
+  end
+
+  def self.find_all_by_given_name(name)
+    Customer.all.select do |person|
+      person.given_name
+    end 
+  end 
+
+  def add_review(restaurant, rating)
+    Review.new(restaurant, self, content)
+  end 
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,13 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+# losgallos = Restaurant.new("los gallos")
+# elfaro = Restaurant.new("elfaro")
+# vasoslokos = Restaurant.new("vasos lokos")
+
+# josue = Customer.new("josue", "viramontes")
+# daniel = Customer.new("daniel", "viramontes")
+# peter = Customer.new("peter", "viramontes")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
