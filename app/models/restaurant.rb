dbabbfacab939b70c1c524f8b71242ff7468e15b
Eve Reichmann <eve.reichmann@gmail.com>
2020-06-30 12:12:32 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,35 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
   end
 
+  def reviews
+    Review.all.select do |my_reviews|
+      my_reviews.restaurant == name
+    end
+  end
+  
+  def customers
+    people = Review.all.select do |my_reviews|
+      my_reviews.restaurant == name
+    end
+    people.map do |peps|
+      peps.customer
+    end  
+  end
+  
+  def average_star_rating
+    num = self.reviews.map do |rate|
+      rate.rating
+    end
+    num.sum / num.size
+  end  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+bob = Customer.new("Bob", "Smith")
+mary = Customer.new("Mary", "Smith")
+joe = Customer.new("Joe", "Smith")
+
+md = Restaurant.new("McD")
+wendy = Restaurant.new("Wendy")
+burger = Restaurant.new("BurgerKing")
+
+one = Review.new("Bob", "McD", 5)
+two = Review.new("Bob", "Wendy", 7)
+three = Review.new("Mary Smith", "McD", 10)
+four = Review.new("Mary", "BurgerKing", 3)
+five = Review.new("Joe", "McD", 8)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
