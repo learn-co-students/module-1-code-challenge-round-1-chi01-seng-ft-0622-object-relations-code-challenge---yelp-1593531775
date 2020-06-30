af037c5ecd020f6037ccd182f44f5d339070cd44
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-06-30 12:12:29 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,31 @@
 class Restaurant
+
+  @@all = []
+
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self }
+  end
+
+  def customers
+    # my_reviews = self.reviews
+    # my_reviews.select.uniq { |review| review.customer }
+    self.reviews.select.uniq { |review| review.customer }
+  end
+
+  def average_star_rating
+    # my_reviews = self.reviews
+    self.reviews.sum { |review| review.rating } / self.reviews.count
   end
 
 end
