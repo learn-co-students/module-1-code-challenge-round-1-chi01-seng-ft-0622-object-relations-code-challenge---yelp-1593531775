c76809406bf967e3bc62a12bc42fd9f87b9cc75e
embraman10@gmail.com <embraman10@gmail.com>
2020-06-30 12:12:30 -0500

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,32 @@
 class Review
   
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+
+    def rating
+        Review.new(customer, restaurant)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def customer_review(customer)
+        self.all.select do |name|
+            name.customer
+        end
+    end
+    
+    def restaurant_review(restaraunt)
+        self.all.select do |location|
+            location.restaraunt
+        end
+    end
+
 end
